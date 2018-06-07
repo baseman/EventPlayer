5d984e9ed5e6a6e0d7912a708d1b0ac66c74660e
baseman <steve.hammond@outlook.com>
2018-06-07 18:32:35 -0400

    fix file / class names

--- src/main/kotlin/co/remotectrl/eventplayer/Player.kt
+++ src/main/kotlin/co/remotectrl/eventplayer/Player.kt
@@ -1,6 +1,6 @@
 package co.remotectrl.eventplayer
 
-class `Player.kt`<TModel: Aggregate<TModel>> {
+class Player<TModel: Aggregate<TModel>> {
     fun playFor(evts: Array<PlayEvent<TModel>>, model: TModel) {
         for(evt in evts){
             evt.applyTo(model)
